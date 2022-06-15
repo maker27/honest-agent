471384b5bfd46832ba181d1a84ea6a78ec2bded8
maker27 <evgeniy@maker27.ru>
2022-06-16 01:21:44 +0300

    add red background into CloseIcon

--- src/components/icons/CloseIcon.tsx
+++ src/components/icons/CloseIcon.tsx
@@ -3,15 +3,16 @@ import React from 'react';
 const CloseIcon: React.FC = () => {
     return (
         <svg
-            width="12"
-            height="12"
-            viewBox="0 0 12 12"
+            width="20"
+            height="20"
+            viewBox="0 0 20 20"
             fill="none"
             xmlns="http://www.w3.org/2000/svg">
+            <rect width="20" height="20" rx="10" fill="#D95151" />
             <path
                 fillRule="evenodd"
                 clipRule="evenodd"
-                d="M1.19995 2.07226L5.1277 6.00001L1.19995 9.92776L2.0722 10.8L5.99995 6.87226L9.9277 10.8L10.8 9.92776L6.8722 6.00001L10.8 2.07226L9.9277 1.20001L5.99995 5.12776L2.0722 1.20001L1.19995 2.07226Z"
+                d="M5.19995 6.07226L9.1277 10L5.19995 13.9278L6.0722 14.8L9.99995 10.8723L13.9277 14.8L14.8 13.9278L10.8722 10L14.8 6.07226L13.9277 5.20001L9.99995 9.12776L6.0722 5.20001L5.19995 6.07226Z"
                 fill="#F7F7F7"
             />
         </svg>
